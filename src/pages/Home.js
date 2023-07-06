49d814efe185ef00049476eb20ea2fba18f2a96b
Muhammad Rafiq <ibab7810@gmail.com>
2023-07-06 11:45:36 +0700

    update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import Herro from '../components/Herro';
-// import Indonesia from '../components/Indonesia';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import axios from 'axios';
 import ENPOINTS from '../utils/constants/enpoints';
 import Regions from '../components/Regions';
@@ -9,27 +8,24 @@ import { useDispatch } from 'react-redux';
 import { updateGlobal } from '../components/Features/CovidSlice/gloSlice';
 
 function Home() {
-  // const [regiCovid, setRegions] = useState([])
-  // const [gloCovid, setglobal] = useState([]);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getGlobal();
+    async function fetchData() {
+      await getGlobal();
+    }
+    fetchData();
   }, []);
 
   async function getGlobal() {
     const response = await axios(ENPOINTS.GLOBAL);
     dispatch(updateGlobal(response.data));
-    // setglobal(response.data);
-    // console.log(response.data);
   }
 
   return (
     <>
       <Herro />
       <DataGlobal title="Global Situation" />
-      {/* <Indonesia global={global} title="indohayu" /> */}
       <Regions title="Situation By Regions" deskripsi="Bacaan Pilihan Covid" />
     </>
   );
